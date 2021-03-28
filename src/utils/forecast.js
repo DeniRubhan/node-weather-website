9b6bb7aba4c6d01188d640154a1ee3dcec226519
Deni Rubhan <denirubhan31@gmail.com>
2021-03-28 15:10:39 +0530

    Changing the Api to climacell

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,15 +2,16 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) =>{
 
-    const url = 'http://api.weatherstack.com/current?access_key=af3f7f81309a0e609895495c2febeb34&query='+latitude+','+longitude+'&units=f'
+    
+    const url = 'https://data.climacell.co/v4/timelines?location='+latitude+','+longitude+'&fields=temperature&fields=visibility&fields=humidity&fields=windSpeed&fields=sunsetTime&fields=sunriseTime&timesteps=1d&units=metric&apikey=tUyTSmINHfXIsMjVQqGmoMFMFhWdwM3p'
     console.log(url)
     request({url, json: true}, (error, { body }) => {
         if(error){
             callback('There is a problem in the connection' , undefined)
-        } else if(body.error) {
-            callback('Unable to find the location. Try another search' , undefined)
+        } else if(body.message) {
+            callback('Unable to find the location. Try another search'+body.type , undefined)
         } else {
-            callback(undefined , body.current.weather_descriptions[0]+' it is currently '+body.current.temperature+' but it feels like '+body.current.feelslike)
+            callback(undefined , body.data.timelines[0].intervals[0].values.temperature+' it is currently '+body.data.timelines[0].intervals[0].values.temperature+' visibility =  '+body.data.timelines[0].intervals[0].values.visibility+' humidity = '+body.data.timelines[0].intervals[0].values.humidity+' windSpeed = '+body.data.timelines[0].intervals[0].values.windSpeed)
         }
 
     })
