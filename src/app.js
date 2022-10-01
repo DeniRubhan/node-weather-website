79d6c2e37f652341554d7ecb4afd63450e6cd511
DeniRubhan <81305912+DeniRubhan@users.noreply.github.com>
2022-10-01 14:00:28 +0530

    Update app.js
    Name Change in the Code

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirPath))
 
 app.get('',(req, res)=>{
     res.render('index', {
-        name:'Deni',
+        name:'Deni Rubhan',
         title:'Weather App'
     })
 })
